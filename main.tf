e132edc5dab4b0087cd1b118ea732ceabe3dd518
himanshu <rathihimanshu@live.com>
2025-12-05 11:55:21 +0530

    lb

--- main.tf
+++ main.tf
@@ -40,12 +40,16 @@ resource "google_compute_region_instance_group_manager" "myappgroup1" {
   base_instance_name         = "mycustomapp"
   region                     = var.region
   distribution_policy_zones  = data.google_compute_zones.allzones.names
+  named_port {
+    name = "http"
+    port = 80
+  }
 
   version {
     instance_template = google_compute_instance_template.temp1.self_link
   }
 
-  target_size  = 2
+#   target_size  = 2
 
   auto_healing_policies {
     health_check      = google_compute_health_check.http-check.self_link
@@ -81,4 +85,41 @@ resource "google_compute_region_autoscaler" "myapp-autoscaler" {
       target = 0.6
     }
   }
+}
+
+# load balancer
+
+resource "google_compute_backend_service" "lb-backend" {
+  name                  = "${var.image-name}-backend-service-external"
+  protocol              = "HTTP"
+  load_balancing_scheme = "EXTERNAL"
+  health_checks = [google_compute_health_check.http-check.self_link]
+  backend {
+    group = google_compute_region_instance_group_manager.myappgroup1.instance_group
+  }
+  port_name = "http"
+
+}
+
+resource "google_compute_global_address" "default" {
+  name = "lb-ip-address"
+}
+resource "google_compute_target_http_proxy" "default" {
+  name        = "http-lb-proxy"
+  provider    = google
+  url_map     = google_compute_url_map.default.id
+}
+resource "google_compute_url_map" "default" {
+  name            = "http-lb-map"
+  default_service = google_compute_backend_service.lb-backend.id
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name                  = "http-lb"
+  provider              = google
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.default.id
+  ip_address            = google_compute_global_address.default.address
 }
