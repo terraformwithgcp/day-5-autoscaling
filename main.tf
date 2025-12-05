ef59e5a3864405a142aad58d1bef8cd3cfe7a102
himanshu <rathihimanshu@live.com>
2025-12-05 11:09:25 +0530

    instance-group

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ data "google_compute_image" "myappimage" {
     project = var.project-id 
 }
 
+data "google_compute_zones" "allzones" {
+    region = var.region  
+}
+
 
 resource "google_compute_instance_template" "temp1" {
   name        = "${var.image-name}-template"
@@ -27,4 +31,38 @@ resource "google_compute_instance_template" "temp1" {
   }
   tags = [ "http-server" ]
 
+}
+
+
+resource "google_compute_region_instance_group_manager" "myappgroup1" {
+  name = "${var.image-name}-igm"
+
+  base_instance_name         = "mycustomapp"
+  region                     = var.region
+  distribution_policy_zones  = data.google_compute_zones.allzones.names
+
+  version {
+    instance_template = google_compute_instance_template.temp1.self_link
+  }
+
+  target_size  = 2
+
+  auto_healing_policies {
+    health_check      = google_compute_health_check.http-check.self_link
+    initial_delay_sec = 60
+  }
+}
+
+resource "google_compute_health_check" "http-check" {
+    name               = "http-basic-check"
+    check_interval_sec = 10
+    timeout_sec        = 5
+    healthy_threshold  = 2
+    unhealthy_threshold = 2
+    
+    http_health_check {
+        port               = 80
+        request_path       = "/"
+    }
+  
 }
